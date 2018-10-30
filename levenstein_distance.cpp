0e81547c5ad146b95d382f6bf4620ab39d69f588
kanishkamisra <menogetusername@gmail.com>
2018-10-30 16:57:17 -0400

    something

--- levenstein_distance.cpp
+++ levenstein_distance.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+#include <string.h>
 
 //[[Rcpp::export]]
 int string_dist(std::string s, std::string t) {
@@ -28,10 +29,49 @@ int string_dist(std::string s, std::string t) {
 			if(s[i - 1] == t[j - 1]) {
 				d(i, j) = d(i - 1, j - 1); //no operation
 			} else {
-				d(i, j) = std::min(d(i - 1, j) + 1, std::min(d(i, j - 1) + 1, d(i - 1, j - 1) + 1));
+				d(i, j) = std::min(d(i - 1, j) + 1, std::min(d(i, j - 1) + 1, d(i - 1, j - 1) + 2));
 			}
 		}
 	}
+
+	std::string str_s ("");
+	std::string str_t ("");
+
+	int i = n;
+	int j = m;
+	int temp = 0;
+
+	while(i != 0 && j != 0) {
+		if(s[i - 1] == t[j - 1])
+			temp = d(i - 1, j - 1);
+		else 
+			temp = d(i - 1, j - 1) + 2;
+
+		if(d(i, j) == temp) {
+			i--;
+			j--;
+			// str_s.append(s[i]);
+			str_s += s[i];
+			// str_t.append(t[j]);
+			str_t += t[j];
+		}
+		else if (d(i, j) == 1 + d(i - 1, j)) {
+			i--;
+			// str_s.append(s[i]);
+			str_s += s[i];
+			// str_t.append("*");
+			str_t += "*";
+		}
+		else if (d(i, j) == 1 + d(i, j - 1)) {
+			j--;
+			// str_s.append("*");
+			str_s += "*";
+			// str_t,append(t[j]);
+			str_t += t[j];
+		}
+	}
+
+	std::cout << str_s << '\n' << str_t << '\n';
 
 	return d(n, m);
 }
